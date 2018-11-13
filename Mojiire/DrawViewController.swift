592521debf7a02483c71f4d05ca096268dd221b8
ave-hikari <ave.nxson@gmail.com>
2018-11-13 23:57:56 +0900

    textfieldに設定しaddTextボタンタップ時にmainImageに載せる処理　デフォルト配置位置と文字色を設定しておく

--- Mojiire/DrawViewController.swift
+++ Mojiire/DrawViewController.swift
@@ -16,14 +16,37 @@ class DrawViewController: UIViewController,UIImagePickerControllerDelegate,UINav
     @IBOutlet weak var addText: UITextField!
     @IBOutlet weak var setText: UIButton!
     
+    var stampLabel: UILabel!
+    
+    var textTempColor: UIColor!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         addText.delegate = self as? UITextFieldDelegate
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         // ライブラリで選択した画像をimageViewのimageにセット
         mainImage.image = tempImage
+    }
+    
+    @IBAction func tapAddTextBtn(_ sender: Any) {
+        // 文字が画面上に設定されている
+        if (self.stampLabel != nil) {
+            
+        } else {
+            // デフォルトの配置位置と文字色を設定し addSubView
+            self.stampLabel = UILabel(frame:CGRect(x: 50, y: 50, width: 120, height: 20))
+            self.stampLabel.text = addText.text
+            
+            self.stampLabel.textColor = UIColor.white
+            self.stampLabel.backgroundColor = UIColor.clear
+            textTempColor = self.stampLabel.textColor
+            
+            self.mainImage.addSubview(self.stampLabel)
+            setText.setTitle("set!", for: UIControlState.normal)
+        }
     }
 }
